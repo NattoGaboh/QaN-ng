4cccfd8573796c2fa2d92ab9dc2f1cbfadd9f4e9
NattoGaboh <renato.gvl10@gmail.com>
2022-05-27 14:43:02 -0500

    Routing|FormGroup

---
+++ FrontEnd/src/app/components/inicio/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  
+  login: FormGroup;
+
+  constructor(private fb:FormBuilder) {
+    this.login = this.fb.group({
+      usuario: ['',Validators.required],
+      password: ['',Validators.required]
+    });
+   }
+
+  ngOnInit(): void {
+  }
+
+  log(): void{
+    console.log(this.login);
+  }
+
+}
