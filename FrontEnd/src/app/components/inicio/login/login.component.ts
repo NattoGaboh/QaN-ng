1bbb8791bf56c4da60091eac27efcd0d4deeecbd
NattoGaboh <renato.gvl10@gmail.com>
2022-05-27 20:12:29 -0500

    Finish RegisterComponent

--- FrontEnd/src/app/components/inicio/login/login.component.ts
+++ FrontEnd/src/app/components/inicio/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Usuario } from '../../../models/usuario';
 
 @Component({
   selector: 'app-login',
@@ -22,6 +23,12 @@ export class LoginComponent implements OnInit {
 
   log(): void{
     console.log(this.login);
+
+    const usuario: Usuario = {
+      nombreUsuario: this.login.value.usuario,
+      passsword: this.login.value.password
+    }
+    console.log(usuario);
   }
 
 }
