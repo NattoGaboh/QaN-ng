af1ba57bc0d35328fc3e4a664069ebec2e2cacf3
NattoGaboh <renato.gvl10@gmail.com>
2022-05-29 17:12:08 -0500

    Add Dashboard

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,17 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChangepasswordComponent } from './components/dashboard/changepassword/changepassword.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { QuestionnairesComponent } from './components/dashboard/questionnaires/questionnaires.component';
 import { BienvenidaComponent } from './components/inicio/bienvenida/bienvenida.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 import { LoginComponent } from './components/inicio/login/login.component';
 import { RegisterComponent } from './components/inicio/register/register.component';
 
 const routes: Routes = [
-  {path: '', redirectTo:'/bienvenidos', pathMatch:'full'},
-  {path: 'bienvenidos', component: BienvenidaComponent},
-  {path: 'register', component: RegisterComponent},
-  {path: 'login', component: LoginComponent},
-  {path: '**', redirectTo:'/bienvenidos', pathMatch:'full'}
+  {path: '', redirectTo:'/inicio', pathMatch:'full'},
+  {path:'inicio', component: InicioComponent, children: [
+    {path: '', component: BienvenidaComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: 'login', component: LoginComponent}
+  ]},
+  {path:'dashboard', component: DashboardComponent, children:[
+    {path: '', component: QuestionnairesComponent},
+    {path: 'changepassword', component: ChangepasswordComponent}
+  ]},
+  {path: '**', redirectTo:'/inicio', pathMatch:'full'}
 ];
-
+  
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -9,6 +9,10 @@ import { InicioComponent } from './components/inicio/inicio.component';
 import { BienvenidaComponent } from './components/inicio/bienvenida/bienvenida.component';
 import { LoginComponent } from './components/inicio/login/login.component';
 import { RegisterComponent } from './components/inicio/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ChangepasswordComponent } from './components/dashboard/changepassword/changepassword.component';
+import { QuestionnairesComponent } from './components/dashboard/questionnaires/questionnaires.component';
+import { NavbarComponent } from './components/dashboard/navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,11 @@ import { RegisterComponent } from './components/inicio/register/register.compone
     InicioComponent,
     BienvenidaComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    DashboardComponent,
+    ChangepasswordComponent,
+    QuestionnairesComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
